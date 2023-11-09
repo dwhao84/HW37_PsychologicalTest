7d215e03f46dca3ababf8d33a59ce3cdbd2aa6d9
Daweihao84 <dwsamurai84@gmail.com>
2023-11-10 00:04:20 +0800

    Working on: PsyChoiceVC: 1.Need to set up the logic when user selected different button and deliver the data to resultVC. -> Not Ok 2. Create a logic when the user tapped the button few times then transfer the data. -> Not Ok
    MapVC:
    1. Add the data location after received data in MapVC.
    
    FInish:
    HomePageVC:
    1. HomePageVC's pageControl set up is done. -> Ok
    2. Swipe Gesture added in HomePageVC. -> Ok
    
    PsyVC:
    1. Get the data from array for selectionButton in PsyChoiceVC. -> OK
    2. Get the contentLabel from an array in PsyChoiceVC. -> Ok
    3. Create a calculation logic for after click the answerButton. -> OK
    
    Data:
    1. Clean up the data for dictionary change to an array. -> Ok

--- HW37_PsychologicalTest/ViewController/PsychologicalTestViewController.swift
+++ HW37_PsychologicalTest/ViewController/PsychologicalTestViewController.swift
@@ -12,17 +12,16 @@ class PsychologicalTestViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
 
-    @IBOutlet weak var selectButtonOne: UIButton!
-    @IBOutlet weak var selectButtonTwo: UIButton!
-    @IBOutlet weak var selectButtonThree: UIButton!
-    @IBOutlet weak var selectButtonFour: UIButton!
+    @IBOutlet weak var answerButtonOne: UIButton!
+    @IBOutlet weak var answerButtonTwo: UIButton!
+    @IBOutlet weak var answerButtonThree: UIButton!
+    @IBOutlet weak var answerButtonFour: UIButton!
 
     @IBOutlet weak var nextButton: UIButton!
 
     var questionIndex: Int = 0
     var answerIndex : Int = 0
 
-    let selectButtonArray: [UIButton] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,15 +39,15 @@ class PsychologicalTestViewController: UIViewController {
         nextButton.isUserInteractionEnabled = true
         nextButton.isEnabled = true
 
-        let selectButtonArray: [UIButton] = [selectButtonOne, selectButtonTwo, selectButtonThree, selectButtonFour]
+        let answerButtonArray: [UIButton] = [answerButtonOne, answerButtonTwo, answerButtonThree, answerButtonFour]
 
-        for selectionButtons in selectButtonArray {
-            selectionButtons.tintColor = UIColor.systemGray5
-            selectionButtons.titleLabel?.textColor = UIColor.lightGray
-            selectionButtons.titleLabel?.adjustsFontSizeToFitWidth = true
-            selectionButtons.isUserInteractionEnabled = true
-            selectionButtons.isEnabled = true
-            view.addSubview(selectionButtons)
+        for answerButtons in answerButtonArray {
+            answerButtons.tintColor = UIColor.systemGray5
+            answerButtons.titleLabel?.textColor = UIColor.lightGray
+            answerButtons.titleLabel?.adjustsFontSizeToFitWidth = true
+            answerButtons.isUserInteractionEnabled = true
+            answerButtons.isEnabled = true
+            view.addSubview(answerButtons)
         }
 
         titleLabel.text = "Question\(questionIndex + 1)"
@@ -61,84 +60,89 @@ class PsychologicalTestViewController: UIViewController {
         contentLabel.adjustsFontSizeToFitWidth = true
         contentLabel.font = UIFont.boldSystemFont(ofSize: 25)
 
-        updateContent()
+        updateTheContentText()
     }
 
-    func updateContent () {
+    func updateTheContentText () {
         // questionLabel text update
         contentLabel.text = questions[questionIndex].question
 
         // selectButtonOne to Four update
-        selectButtonOne.setTitle(questions[questionIndex].answers[0].text, for: .normal)
-        selectButtonTwo.setTitle(questions[questionIndex].answers[1].text, for: .normal)
-        selectButtonThree.setTitle(questions[questionIndex].answers[2].text, for: .normal)
-        selectButtonFour.setTitle(questions[questionIndex].answers[3].text, for: .normal)
+        answerButtonOne.setTitle(questions[questionIndex].answers[0].text, for: .normal)
+        answerButtonTwo.setTitle(questions[questionIndex].answers[1].text, for: .normal)
+        answerButtonThree.setTitle(questions[questionIndex].answers[2].text, for: .normal)
+        answerButtonFour.setTitle(questions[questionIndex].answers[3].text, for: .normal)
     }
 
     // Build the logic after the nextQuestionButtonTapped
-    func questionCount () {
+    func questionIndexLogic () {
         let countForQuestion = questions.count - 1
         print(questionIndex)
         if questionIndex < countForQuestion {
-            questionIndex = Int.random(in: 0...countForQuestion)
+            questionIndex += 1
         } else {
-           questionIndex -= 1
+           // peformSegue
+
         }
     }
 
 
     // 建立一個底層邏輯去判斷sender.title的內容是否符合combineChoicesData的內容
     // if yes 所選的城市 +10分，選分最高的城市 將選擇的城市 座標傳到 imaps.
-    @IBAction func tapSelectButtonOne(_ sender: UIButton) {
+    @IBAction func answerButtonOneTapped(_ sender: UIButton) {
         print(questionIndex)
         print("tpaSelectButtonOne")
-        questionCount()
+        questionIndexLogic()
 
         // Rest of Buttons will be is NOT Enabled
-        selectButtonTwo.isEnabled = false
-        selectButtonThree.isEnabled = false
-        selectButtonFour.isEnabled = false
+        answerButtonTwo.isEnabled = false
+        answerButtonThree.isEnabled = false
+        answerButtonFour.isEnabled = false
     }
 
     @IBAction func tapSelectButtonTwo(_ sender: UIButton) {
         print(questionIndex)
         print("tapSelectButtonTwo")
-        questionCount()
+        questionIndexLogic()
 
         // Rest of Buttons will be is NOT Enabled
-        selectButtonOne.isEnabled = false
-        selectButtonThree.isEnabled = false
-        selectButtonFour.isEnabled = false
+        answerButtonOne.isEnabled = false
+        answerButtonThree.isEnabled = false
+        answerButtonFour.isEnabled = false
     }
 
     @IBAction func tapSelectButtonThree(_ sender: UIButton) {
         print(questionIndex)
         print("tapSelectButtonThree")
-        questionCount()
+        questionIndexLogic()
 
         // Rest of Buttons will be is NOT Enabled
-        selectButtonOne.isEnabled = false
-        selectButtonTwo.isEnabled = false
-        selectButtonFour.isEnabled = false
+        answerButtonOne.isEnabled = false
+        answerButtonTwo.isEnabled = false
+        answerButtonFour.isEnabled = false
     }
 
     @IBAction func tapSelectButtonFour(_ sender: UIButton) {
         print(questionIndex)
         print("tapSelectButtonThree")
-        questionCount()
+        questionIndexLogic()
 
         // Rest of Buttons will be is NOT Enabled
-        selectButtonOne.isEnabled = false
-        selectButtonTwo.isEnabled = false
-        selectButtonThree.isEnabled = false
+        answerButtonOne.isEnabled = false
+        answerButtonTwo.isEnabled = false
+        answerButtonThree.isEnabled = false
     }
 
-    @IBAction func tapNextPageButton(_ sender: UIButton) {
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
         print("tapNextPageButton")
-        questionCount ()
 
-        updateContent()
+        questionIndexLogic ()
+        updateTheContentText()
 
+        let answerButtonArray: [UIButton] = [answerButtonOne, answerButtonTwo, answerButtonThree, answerButtonFour]
+        for answerButtons in answerButtonArray {
+            answerButtons.isEnabled = true
+        }
     }
 
 
